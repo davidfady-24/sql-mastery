acb85a1c6059326caf0523afc430e056f2b01435
David Fady <david.fady24@gmail.com>
2026-06-01 12:35:06 +0300

    Add SQL queries for LeetCode problems

--- leetcode-solutions.sql
+++ leetcode-solutions.sql
@@ -13,3 +13,21 @@ SELECT (
     ORDER BY salary DESC
     LIMIT 1 OFFSET 1
 ) AS SecondHighestSalary;
+
+
+--LeetCode 178. Rank Scores
+
+SELECT
+    score,
+    DENSE_RANK() OVER (
+        ORDER BY score DESC
+    ) AS `rank`
+FROM Scores
+ORDER BY score DESC;
+
+--181. Employees Earning More Than Their Managers
+SELECT e2.name as Employee
+FROM employee e1
+INNER JOIN employee e2 ON e1.id = e2.managerID
+WHERE
+e1.salary < e2.salary
